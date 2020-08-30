fa6dd5f30f72b75a908ccb2222a20fd351295900
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-08-30 17:24:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -57,7 +57,7 @@ class Song
     song
   end
   
-  def self.create_from_filename
+  def self.create_from_filename(filename)
     result = self.new_from_filename(filename)
     song = self.create
     song.name = result.name
